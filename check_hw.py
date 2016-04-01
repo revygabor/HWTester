5b5be13beccdef7d48075994bcd6ddbf88e614d5
hunsteve <hunsteve@gmail.com>
2016-04-01 15:37:43 +0200

    continuous running

--- check_hw.py
+++ check_hw.py
@@ -5,19 +5,6 @@ import time
 from connection_hfportal import get_new_submissions, get_submission_file, post_result
 from prepare_hw import unzip, java_compile, run_firejail_java, java_compile_all, clean_dir, find_java_class_with_package
 
-
-with open('config.json') as config_file:  
-    config = json.load(config_file)
-    hw_data = config['homeworkData']
-    WORKING_DIR = config['workingDir']
-    EXTRACT_DIR = config['extractDirPrefix']    
-    CORRECTOR_NAME = config['appName']
-    MESSAGE_MAX_LENGTH = config['messageMaxLength']
-    break_on_first_error = config['breakOnFirstError']    
-    log_to_html = config['logToHTML']
-    enable_write_to_database = config['enableWriteToDatabase']
-
-
 def truncate(s):
     if len(s) > MESSAGE_MAX_LENGTH:
         return "[...]%s" % s[-MESSAGE_MAX_LENGTH:]
@@ -115,68 +102,82 @@ def log2html(log):
                 file.write("</tr>")
             file.write("</table>")
 
-for hw_id, hw_details in hw_data.iteritems():
-    if isinstance(hw_details['testcases'], basestring):
-        with open(hw_details['testcases'],"r") as data_file:        
-            hw_details['testcases'] = json.load(data_file)
 
-    for submission_id in get_new_submissions(hw_id):
-        print("Testing submission: %d" % submission_id)
-
-        log = {}
-        log["id"] = submission_id
-
-        data = get_submission_file(submission_id)
-        
-        src_dir = EXTRACT_DIR + str(submission_id)
-
-        clean_dir(src_dir)
-        clean_dir(WORKING_DIR)
-
-        try:
-            unzip(data, src_dir)
-            (stdout, stderr) = java_compile_all(src_dir, WORKING_DIR)
-            if stderr:
-                log["compile_error"] = stderr   
-        except Exception as e:
-            log["compile_error"] = e                            
-
-        if "compile_error" not in log:
-            target = find_java_class_with_package(hw_details['classname'], WORKING_DIR)       
-            log["classname"] = target            
-
-            testcntr = 0
-            log["starttime"] = time.clock()
-            for data in hw_details['testcases']:            
-                input = data[0]
-                target_output = data[1]
-
-                testcntr += 1
-                if (testcntr > hw_details["max testcount"]):
-                    break
-                (stdout, stderr, extraerr) = run_firejail_java(target,input,hw_details['firejail profile'], timeout = hw_details['timeout'])
-                stdout = stdout.strip()
-                target_output = target_output.strip()
-
-                log_output(log, input, target_output, stdout, stderr or extraerr)               
-                
-                if break_on_first_error and (stderr or extraerr):                
-                    break                                            
-                
-            log["endtime"] = time.clock()
-
-            if enable_write_to_database:
-                (err, errinp) = check_log_for_errors(log)
-                if err and errinp:
-                    errtext = "Runtime error occured \n%s\nfor input: \n%s" % (truncate(err), errinp)
-                    post_result(submission_id, CORRECTOR_NAME, 9, 0, errtext)
-                else:
-                    post_result(submission_id, CORRECTOR_NAME, 7, score_from_log(log, hw_details['scorelimits']), response_message_from_log(log))
-        else:
-            if enable_write_to_database:
-                post_result(submission_id, CORRECTOR_NAME, 9, 0, truncate(log["compile_error"]))            
-        if log_to_html:
-            log2html(log)    
+while True:
+    with open('config.json') as config_file:  
+        config = json.load(config_file)
+        hw_data = config['homeworkData']
+        WORKING_DIR = config['workingDir']
+        EXTRACT_DIR = config['extractDirPrefix']    
+        CORRECTOR_NAME = config['appName']
+        MESSAGE_MAX_LENGTH = config['messageMaxLength']
+        break_on_first_error = config['breakOnFirstError']    
+        log_to_html = config['logToHTML']
+        enable_write_to_database = config['enableWriteToDatabase']
+        is 'stop' in config:
+            break
 
 
-       
+    for hw_id, hw_details in hw_data.iteritems():
+        if isinstance(hw_details['testcases'], basestring):
+            with open(hw_details['testcases'],"r") as data_file:        
+                hw_details['testcases'] = json.load(data_file)
+
+        for submission_id in get_new_submissions(hw_id):
+            print("Testing submission: %d" % submission_id)
+
+            log = {}
+            log["id"] = submission_id
+
+            data = get_submission_file(submission_id)
+            
+            src_dir = EXTRACT_DIR + str(submission_id)
+
+            clean_dir(src_dir)
+            clean_dir(WORKING_DIR)
+
+            try:
+                unzip(data, src_dir)
+                (stdout, stderr) = java_compile_all(src_dir, WORKING_DIR)
+                if stderr:
+                    log["compile_error"] = stderr   
+            except Exception as e:
+                log["compile_error"] = e                            
+
+            if "compile_error" not in log:
+                target = find_java_class_with_package(hw_details['classname'], WORKING_DIR)       
+                log["classname"] = target            
+
+                testcntr = 0
+                log["starttime"] = time.clock()
+                for data in hw_details['testcases']:            
+                    input = data[0]
+                    target_output = data[1]
+
+                    testcntr += 1
+                    if (testcntr > hw_details["max testcount"]):
+                        break
+                    (stdout, stderr, extraerr) = run_firejail_java(target,input,hw_details['firejail profile'], timeout = hw_details['timeout'])
+                    stdout = stdout.strip()
+                    target_output = target_output.strip()
+
+                    log_output(log, input, target_output, stdout, stderr or extraerr)               
+                    
+                    if break_on_first_error and (stderr or extraerr):                
+                        break                                            
+                    
+                log["endtime"] = time.clock()
+
+                if enable_write_to_database:
+                    (err, errinp) = check_log_for_errors(log)
+                    if err and errinp:
+                        errtext = "Runtime error occured \n%s\nfor input: \n%s" % (truncate(err), errinp)
+                        post_result(submission_id, CORRECTOR_NAME, 9, 0, errtext)
+                    else:
+                        post_result(submission_id, CORRECTOR_NAME, 7, score_from_log(log, hw_details['scorelimits']), response_message_from_log(log))
+            else:
+                if enable_write_to_database:
+                    post_result(submission_id, CORRECTOR_NAME, 9, 0, truncate(log["compile_error"]))            
+            if log_to_html:
+                log2html(log)    
+    time.sleep(60)
