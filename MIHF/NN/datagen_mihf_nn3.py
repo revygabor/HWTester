43898c4a58f35dc5c29ed07949112c0f0df949cf
Engedy István <engedy@mit.bme.hu>
2016-10-01 19:51:49 +0200

    NN homework part 3 test cases updated

--- MIHF/NN/datagen_mihf_nn3.py
+++ MIHF/NN/datagen_mihf_nn3.py
@@ -8,6 +8,7 @@ inps = ["1,2,1","1,1","2,3,2,1","1,4,6,1","3,1","2,6,2,3,4,1"] #make sure output
 to_json = []
 
 to_json.append({"input":"2,3,1\n1,0,-0.5\n0,1,-0.5\n1,1,-1\n2,2,-2,0\n1\n0.75,0.75\n\n"})
+to_json.append({"input":"1,1,1\n1,1\n1,1\n1\n-1\n\n"})
 
 
 for inp in inps:
