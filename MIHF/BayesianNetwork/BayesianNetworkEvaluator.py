9bb378d444c04ece6113ddf6df3bd6260818bea6
Vetró Mihály <vetromisu@gmail.com>
2020-10-09 14:26:32 +0200

    Resolve argument name conflict in the BayesianNetwork homework.

--- MIHF/BayesianNetwork/BayesianNetworkEvaluator.py
+++ MIHF/BayesianNetwork/BayesianNetworkEvaluator.py
@@ -19,8 +19,8 @@ class BayesianNetworkEvaluator(Evaluator.Evaluator):
                 return (0, "Expected {0} probability values but received {1} for input:\n\n{2}".format(
                     str(len(target_output_values)), str(len(output_values)), input_str
                 ))
-            for target, output in zip(target_output_values, output_values):
-                if abs(target - output) > 0.0001:
+            for target, current_output in zip(target_output_values, output_values):
+                if abs(target - current_output) > 0.0001:
                     return (0, "One or more probabilities exceeded the 0.0001 error treshold for "
                                "target output:\n\n{1}\n\nactual output:\n\n{2}\n\ninput:\n\n{0}".format(
                         input_str, target_output, output
